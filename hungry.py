2e3d2d436a3502f0a2ed3e2a97bc5943d115728b
varshinibalachand <varshiniyehsuha@gmail.com>
2022-04-08 12:42:11 +0530

    water

---
+++ hungry.py
@@ -0,0 +1,9 @@
+hungry=input("do you feel hungry?")
+if hungry=="yes":
+    print("eat pizza")
+    print("eat sandwich")
+else:
+    thirsty=input("do you feel thirsty?")
+    if thirsty=="yes":
+        print("Drink some juice")
+
